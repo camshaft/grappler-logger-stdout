9980f38daa3a1eba8b24ae33d4bab7d65fa15bb8
camshaft <bytheway.cameron@gmail.com>
2014-06-03 03:25:26 -0600

    update repo

--- index.js
+++ index.js
@@ -8,7 +8,8 @@
 module.exports = function(config) {
   return function(app) {
     app.logger(function(ns, level, str, task) {
-      var prefix = ns + ':' + level + ' [' + task.repo + '#' + task.branch + ']';
+      var info = task.info;
+      var prefix = ns + ':' + level + ' [' + info.repo + '#' + info.branch + '] ';
       str.split("\n").forEach(function(line) {
         var out = prefix + line;
         if (ns === 'progress') return process.stdout.write(out);
